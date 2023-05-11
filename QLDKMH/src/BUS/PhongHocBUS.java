bd3d808674ad460b4ff476448c07ebf31166cf12
LTHuu <117341204+LTHuu@users.noreply.github.com>
2023-05-11 08:41:21 +0700

    Add files via upload

---
+++ QLDKMH/src/BUS/PhongHocBUS.java
@@ -0,0 +1,74 @@
+package BUS;
+
+import java.util.ArrayList;
+
+import DTO.PhongHoc;
+
+public class PhongHocBUS {
+    static ArrayList<PhongHoc> dsPH = new ArrayList<PhongHoc>();
+    static int slPH = 0;
+
+    public PhongHocBUS() {
+
+    }
+
+    public boolean them(PhongHoc obj) {
+        if (checkTrung(obj)) {
+            dsPH.add(obj);
+            slPH++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkTrung(PhongHoc obj) {
+        for (PhongHoc temp : dsPH) {
+            if (temp.getMaPH().equals(obj.getMaPH())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void sua(PhongHoc old_obj, PhongHoc new_obj) {
+        dsPH.set(dsPH.indexOf(old_obj), new_obj);
+    }
+
+    public void xoa(PhongHoc obj) {
+        dsPH.remove(dsPH.indexOf(obj));
+        slPH--;
+    }
+
+    public ArrayList<PhongHoc> tim(String str) {
+        ArrayList<PhongHoc> temp = new ArrayList<PhongHoc>();
+        for (PhongHoc t : dsPH) {
+            boolean ok = false;
+            if (t.getMaPH().indexOf(str) != -1) {
+                ok = true;
+            }
+            if (!ok && t.getTenPH().indexOf(str) != -1) {
+                ok = true;
+            }
+
+            if (ok)
+                temp.add(t);
+        }
+        return temp;
+    }
+
+    public static ArrayList<PhongHoc> getDsPH() {
+        return dsPH;
+    }
+
+    public static int getSlPH() {
+        return slPH;
+    }
+
+    public static void setDsPH(ArrayList<PhongHoc> dsPH) {
+        PhongHocBUS.dsPH = dsPH;
+    }
+
+    public static void setSlPH(int slPH) {
+        PhongHocBUS.slPH = slPH;
+    }
+}
